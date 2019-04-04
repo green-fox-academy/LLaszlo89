dae65c783795b9247c84710ad49eab1f075c8db2
Lehel Laszlo <l.eli.12@hotmail.com>
2019-04-04 16:46:37 +0200

    introduce

---
+++ Week01/Day4/introduce.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log(
+    "Lehel Laszlo"
+);
+console.log(
+    "30"
+);console.log(
+    "1.67"
+);
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
