25034f7643f67d47c57f91257d86daf6f0779ed7
Lehel Laszlo <l.eli.12@hotmail.com>
2019-04-04 22:17:31 +0200

    define-basic-info

---
+++ Week01/Day4/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+
+let MyName: string = "Lehel";
+let MyAge: number = 30;
+let MyHeight: number = 1.67;
+let married: boolean = false;
+
+console.log(
+MyAge , MyName , MyHeight, married
+
+);
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
