433ee40e6968587f872204d444cdb18fd4d8c1b5
Lehel Laszlo <l.eli.12@hotmail.com>
2019-04-05 10:31:10 +0200

    cuboid

---
+++ Week01/Day5/cuboid.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let l = 5 ;
+let b = 5 ;
+let h = 10 ;
+
+console.log("Surface Area: "+ 2 * ( l + b +h ) + "m"); 
+console.log("Surface Area: ", 2 * ( l + b +h ), "m"); 
+console.log(`Surface Area: ${ 2 * ( l + b +h )} m`); 
+
+console.log(`Volume: ${ l * b * h }`);
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
