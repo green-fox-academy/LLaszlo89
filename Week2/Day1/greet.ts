01e22d2fd2e78b7c5ed7d826b65217e5818c2a8e
Lehel Laszlo <l.eli.12@hotmail.com>
2019-04-08 22:06:29 +0200

    greet

---
+++ Week2/Day1/greet.ts
@@ -0,0 +1,22 @@
+'use strict'
+
+let nemToGreet: string = "Greenfox";
+
+function greet ( name ) {
+    console.log('Greetings, dear ' + name);
+    
+    
+    
+}
+greet('Dalma');
+
+
+
+
+
+
+// -  Create variable named `nameToGreet` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `nameToGreet`
---
+++ Week2/Day1/valami.ts
@@ -0,0 +1,4 @@
+'use strict';
+
+var str = new String( "This is string" ); 
+console.log("str.constructor is:" + str.constructor)
