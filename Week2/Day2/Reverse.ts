17056a108671f9fc090668a1514fba5b0b1572a8
Lehe Laszlo <l.eli.12@hotmail.com>
2019-04-09 13:28:20 +0200

    Reverse

---
+++ Week2/Day2/Reverse.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+function reverse(strreversed ): string {
+ 
+    return strreversed.split ("").reverse().join("");
+}
+
+let reversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+console.log(reverse(reversed));
+
+
+
+export = reverse;
+
+
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
---
+++ Week2/Day2/simple-replace.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+let example: string = 'In a dishwasher far far away';
+let re: string = 'dishwasher';
+let newstr = example.replace (re, 'galaxy' );
+console.log(newstr);
+
+
+
+
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
