cf308a08efa2a929ee99ab43f3d37f58ce235a19
Lehe Laszlo <l.eli.12@hotmail.com>
2019-04-13 11:12:38 +0200

    print-params

---
+++ Week2/Day1/print-params.ts
@@ -0,0 +1,14 @@
+//Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+
+
+
+
+  function printParams( one:string='szia' , two:number=12, three:boolean= true) {
+    console.log(one + two + three);
+    
+  }
+
+  printParams();
