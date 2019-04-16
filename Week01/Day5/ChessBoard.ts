088be72e172357075309c727e4785519c42b10f0
Lehe Laszlo <l.eli.12@hotmail.com>
2019-04-16 12:08:27 +0200

    updateChessBoard

--- Week01/Day5/ChessBoard.ts
+++ Week01/Day5/ChessBoard.ts
@@ -1,61 +1,21 @@
     
 'use strict';
 
-var chessBoard = function(size){
-  var elementBlack = '%'
-  var elementWhite = ' '
-  for (var i = 0; i < size; i++) 
-  {        
 
-      elementBlack += ' '
-
-      for (var j = 0; j < size; j++) 
-      {
-      if((i +j) % 2 == 0)
-      {
-      elementBlack  += elementWhite
-      }
-      else
-      {
-      elementBlack  += "%"
-      }
-  };
-
-};
-
-console.log(elementBlack)
-
-}(8);
-
-
-/*
-let borderSize:number=8;
-let elementBlack: string= "%";
-let elementWhite: string= " ";
-
-for (let i=0; i<=borderSize; i++){
-  elementBlack += " "
-
-   for (let j =0; j<=borderSize; j++){
-     /*
-if ( ( i+ j) % 2 ==0 ){
-  console.log(elementBlack);
+for(let i = 0; i < 8; i++){
+  let row = '';
   
-} else {
-  console.log(elementWhite);
-  
+  for(let j = 0; j < 8; j++){
+    if(j % 2 === 0) {
+      row += 'X';
+    } else {
+      row += ' ';
+    }
+  }
+
+  if(i%2 === 0){
+    row = row.split('').reverse().join('');
+  }
+
+  console.log(row);
 }
-  } */
-
-
-// Create a program that draws a chess table like this
-//
-// % % % %
-//  % % % %
-// % % % %
-//  % % % %
-// % % % %
-//  % % % %
-// % % % %
-//  % % % %
-// */
