8182980732d5bec693a9c9c223035a354986d022
Lehe Laszlo <l.eli.12@hotmail.com>
2019-04-17 19:54:20 +0200

    'Greet'

--- Week2/Day1/greet.ts
+++ Week2/Day1/greet.ts
@@ -2,6 +2,12 @@
 
 let nameToGreet: string = 'GreenFox';
 
+function greet(input?:string){
+ input === undefined ? console.log(nameToGreet) : console.log('Greetings, dear ' + input);
+
+}
+greet();
+
 
 // -  Create variable named `nameToGreet` and assign the value `Greenfox` to it
 // -  Create a function called `greet` that greets it's input parameter
