cac0d392b23e133389c3d469877cce44bb47f50c
Lehe Laszlo <l.eli.12@hotmail.com>
2019-04-18 13:59:21 +0200

    sum-all

---
+++ Week2/Day1/sum-all.ts
@@ -0,0 +1,10 @@
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
+
+let ai=[3, 4, 5, 6, 7];
+let result = 0;
+
+
+for(let i=0; i<+ai.length; i++)
+   result = result + ai[i];
+console.log(result);
