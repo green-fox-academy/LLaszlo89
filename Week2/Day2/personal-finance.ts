1df5b303e556b86fe7374877df8e5f17372a99b0
Lehe Laszlo <l.eli.12@hotmail.com>
2019-04-23 17:33:22 +0200

    GFO

--- Week2/Day2/personal-finance.ts
+++ Week2/Day2/personal-finance.ts
@@ -21,14 +21,12 @@ console.log(allSpendings);
 list.sort(function(a, b) {
   return a - b;
 });
+console.log(list);
+
 console.log('The cheapest spending was: '+ list[0]);
 console.log('The greatest spending was: '+ list[5]);
 
-
-for( let i=0; i<list.length; i++){
-  (allSpendings += list[i])
-}
-console.log('The average: '+ allSpendings / list.length );
+console.log('The average: '+  allSpendings / list.length );
 
 /* var numbers = [4, 2, 5, 1, 3];
 numbers.sort(function(a, b) {
---
+++ Week4/Day1/Garden/garden.ts
@@ -0,0 +1,20 @@
+'use strict'
+
+export class garden {
+  color: string;
+  type: string;
+  currentWaterNeeds: number;
+  minIntake: number;
+
+constructor(currentWaterNeeds = 110){
+this.currentWaterNeeds = currentWaterNeeds;
+
+
+}
+
+
+
+
+
+}
+
